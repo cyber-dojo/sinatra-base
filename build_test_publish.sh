e35b4906138bb9753d4a1089fdf65ae324fb6208
Jon Jagger <jon@jaggersoft.com>
2020-11-12 20:59:20 +0000

    Moved commands to push tagged images into .sh file.

--- build_test_publish.sh
+++ build_test_publish.sh
@@ -2,7 +2,7 @@
 
 readonly SH_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/sh" && pwd)"
 source "${SH_DIR}/build_docker_image.sh"
-#source "${SH_DIR}/on_ci_publish_tagged_images.sh"
+source "${SH_DIR}/on_ci_publish_tagged_images.sh"
 
 build_docker_image
-#on_ci_publish_tagged_images
+on_ci_publish_tagged_images
--- sh/build_docker_image.sh
+++ sh/build_docker_image.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -Eeu
-#set -x
+
 readonly ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 
 # - - - - - - - - - - - - - - - - - - - - - -
