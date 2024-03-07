e2b3f6331b26afd98948a5e1afb5a6cf7b429caf
JonJagger <jon@kosli.com>
2024-03-07 07:44:02 +0000

    Turn off kosli reporting

--- build_test_publish.sh
+++ build_test_publish.sh
@@ -3,11 +3,11 @@ set -Eeu
 
 export REPO_ROOT="$(git rev-parse --show-toplevel)"
 export SH_DIR="${REPO_ROOT}/sh"
-source "${SH_DIR}/kosli.sh"
+#source "${SH_DIR}/kosli.sh"
 source "${SH_DIR}/lib.sh"
 
 build_docker_image
-on_ci_publish_tagged_image
-on_ci_kosli_create_flow
-on_ci_kosli_report_artifact
-on_ci_kosli_report_snyk_evidence
+#on_ci_publish_tagged_image
+#on_ci_kosli_create_flow
+#on_ci_kosli_report_artifact
+#on_ci_kosli_report_snyk_evidence
