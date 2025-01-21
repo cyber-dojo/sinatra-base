92a5c4c05d167694682767b7811b4eeeb8227eb3
JonJagger <jon@kosli.com>
2025-01-21 02:11:34 +0000

    Remove notify-dependents from workflow

--- sh/image_push.sh
+++ sh/image_push.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 set -Eeu
 
-export REPO_ROOT="$(git rev-parse --show-toplevel)"
-source "${REPO_ROOT}/sh/lib.sh"
-
 echo "${DOCKER_PASS}" | docker login --username "${DOCKER_USER}" --password-stdin
-docker push $(image_name):latest
-docker push $(image_name):$(image_tag)
+docker push "${IMAGE_NAME}"
 docker logout
