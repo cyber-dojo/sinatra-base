d8b8a99dff4d79fc27fce2b67b960e477ac86398
JonJagger <jon@kosli.com>
2025-05-03 11:05:19 +0100

    Update workflow to create pr on downstream repos

--- sh/build_image.sh
+++ sh/build_image.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 set -Eeu
 
-export REPO_ROOT="$(git rev-parse --show-toplevel)"
-source "${REPO_ROOT}/sh/lib.sh"
+build_image()
+{
+  local -r repo_root="$(git rev-parse --show-toplevel)"
+  local -r commit_sha="$(git rev-parse HEAD)"
+  local -r image_tag="${commit_sha:0:7}"
+  docker build \
+    --build-arg "COMMIT_SHA=${commit_sha}" \
+    --tag "cyberdojo/sinatra-base:${image_tag}" \
+    "${repo_root}"
+}
 
 build_image
