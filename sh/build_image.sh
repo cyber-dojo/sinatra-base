eca43051053b3280c91c489cdeab8fcfbbd2ea33
JonJagger <jon@kosli.com>
2025-05-04 10:50:47 +0100

    Restore build-image script to last known working version

--- sh/build_image.sh
+++ sh/build_image.sh
@@ -1,15 +1,7 @@
 #!/usr/bin/env bash
 set -Eeu
 
-build_image()
-{
-  local -r repo_root="$(git rev-parse --show-toplevel)"
-  local -r commit_sha="$(git rev-parse HEAD)"
-  local -r image_tag="${commit_sha:0:7}"
-  docker build \
-    --build-arg "COMMIT_SHA=${commit_sha}" \
-    --tag "cyberdojo/sinatra-base:${image_tag}" \
-    "${repo_root}"
-}
+export REPO_ROOT="$(git rev-parse --show-toplevel)"
+source "${REPO_ROOT}/sh/lib.sh"
 
 build_image
